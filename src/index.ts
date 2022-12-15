8bec8ce4bf328ec8c92208355f7b2e170a0b884f
Martin <robmar18@protiviti.com>
2022-12-15 09:35:59 -0500

    initial

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import bodyParser from "body-parser";
+import express from "express";
+import pg from "pg";
+
+// Connect to the database using the DATABASE_URL environment
+//   variable injected by Railway
+const pool = new pg.Pool();
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(bodyParser.json());
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+app.use(bodyParser.text({ type: "text/html" }));
+
+app.get("/", async (req, res) => {
+  const { rows } = await pool.query("SELECT NOW()");
+  res.send(`Rodj says HELLOPUSH from the root of this app. URL=${process.env.DATABASE_URL} | The time from the DB is ${rows[0].now}`);
+});
+
+app.listen(port, () => {
+  console.log(`This is the Rodj example app listening at http://localhost:${port}`);
+});
